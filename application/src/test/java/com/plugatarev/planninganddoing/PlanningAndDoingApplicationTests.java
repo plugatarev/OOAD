7dd211bc10a1ee9fca0ac794c451d41f6ebd18f8
plugatarev <plugatarevk@gmail.com>
2022-09-25 13:05:18 +0700

    source code

---
+++ application/src/test/java/com/plugatarev/planninganddoing/PlanningAndDoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plugatarev.planninganddoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningAndDoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
