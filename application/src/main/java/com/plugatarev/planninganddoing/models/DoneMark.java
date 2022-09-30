e4bc3f561bd2fd926b889a0ef3b56a510c8983fb
plugatarev <plugatarevk@gmail.com>
2022-09-30 19:34:18 +0700

    marks

--- application/src/main/java/com/plugatarev/planninganddoing/models/DoneMark.java
+++ application/src/main/java/com/plugatarev/planninganddoing/models/DoneMark.java
@@ -1,4 +1,18 @@
 package com.plugatarev.planninganddoing.models;
 
-public class Mark {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity(name = "DoneMarkTable")
+public class DoneMark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String anons;
+    private String fullText;
 }
--- application/src/main/java/com/plugatarev/planninganddoing/models/TrashMark.java
+++ application/src/main/java/com/plugatarev/planninganddoing/models/TrashMark.java
@@ -1,2 +1,19 @@
-package com.plugatarev.planninganddoing.models;public class TrashMark {
+package com.plugatarev.planninganddoing.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity(name = "TrashMarkTable")
+public class TrashMark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String anons;
+    private String fullText;
+    private Date dataExecution;
 }
